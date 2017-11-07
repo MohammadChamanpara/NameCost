15e3bb43720febf3a2ad6438f2c6fb84bb85348a
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2017-11-07 19:11:24 +0800

    Add unit tests for currency class

---
+++ src/NameCost.Logic.Tests/CurrencyTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace NameCost.Logic.Tests
+{
+	[TestClass]
+	public class CurrencyTests
+	{
+		[TestMethod]
+		public void ToWord_WithDollarAndCent_ShouldConcat()
+		{
+			//Arrange
+			var currency = new Currency(123456.45m);
+
+			//Act
+			var words = currency.ToWords();
+
+			//Assert
+			words.ToUpper().ShouldBeEquivalentTo("ONE HUNDRED AND TWENTY-THREE THOUSAND FOUR HUNDRED AND FIFTY-SIX DOLLARS AND FORTY-FIVE CENTS");
+		}
+		[TestMethod]
+		public void ToWord_WithoutCents_ShouldAddOnly()
+		{
+			//Arrange
+			var currency = new Currency(100);
+
+			//Act
+			var words = currency.ToWords();
+
+			//Assert
+			words.ToUpper().ShouldBeEquivalentTo("ONE HUNDRED DOLLARS ONLY");
+		}
+
+		[TestMethod]
+		public void ToWord_WithoutDollars_ShouldUseCents()
+		{
+			//Arrange
+			var currency = new Currency(0.25m);
+
+			//Act
+			var words = currency.ToWords();
+
+			//Assert
+			words.ToUpper().ShouldBeEquivalentTo("TWENTY-FIVE CENTS");
+		}
+
+		[TestMethod]
+		public void ToWord_WithOneDollar_ShouldUseSingularForm()
+		{
+			//Arrange
+			var currency = new Currency(1.25m);
+
+			//Act
+			var words = currency.ToWords();
+
+			//Assert
+			words.ToUpper().ShouldBeEquivalentTo("ONE DOLLAR AND TWENTY-FIVE CENTS");
+		}
+
+
+		[TestMethod]
+		public void ToWord_WithOneCent_ShouldUseSingularForm()
+		{
+			//Arrange
+			var currency = new Currency(12.01m);
+
+			//Act
+			var words = currency.ToWords();
+
+			//Assert
+			words.ToUpper().ShouldBeEquivalentTo("TWELVE DOLLARS AND ONE CENT");
+		}
+		[TestMethod]
+		public void ToWord_WithBigNumber_ShouldConvert()
+		{
+			//Arrange
+			var currency = new Currency(2000000000.00m);
+
+			//Act
+			var words = currency.ToWords();
+
+			//Assert
+			words.ToUpper().ShouldBeEquivalentTo("TWO BILLION DOLLARS ONLY");
+		}
+	}
+}
---
+++ src/NameCost.Logic.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NameCost.Logic.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NameCost.Logic.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("908500bc-0a98-4121-b250-12d00201a265")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
