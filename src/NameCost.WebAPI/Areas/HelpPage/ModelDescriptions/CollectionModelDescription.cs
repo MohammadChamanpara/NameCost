8f0e6fde37909d5b17664a06f51664a71dea4205
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2017-11-07 19:23:08 +0800

    Remove HelpPages from webapi project

--- src/NameCost.WebAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace NameCost.WebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- src/NameCost.WebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace NameCost.WebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
