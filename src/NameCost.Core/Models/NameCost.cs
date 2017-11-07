31674abba92986d44af096630693969fea48930c
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2017-11-07 09:31:37 +0800

    Add NameCost model

---
+++ src/NameCost.Core/Models/NameCost.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameCost.Core
+{
+	/// <summary>
+	/// This models servers as DomainModel and ViewModel in this application
+	/// for the sake of simplicity given the fact that the mentioned models 
+	/// had no or slight differences in terms of strucure.
+	/// </summary>
+	public class NameCost
+    {
+		/// <summary>
+		/// Gets or sets the Name property.
+		/// </summary>
+		/// <value>
+		/// The calculated short URL.
+		/// </value>
+		[Display(Name = "Person Name")]
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Gets or sets the cost.
+		/// </summary>
+		/// <value>
+		/// The cost in number format.
+		/// </value>
+		
+		[Display(Name ="Cost")]
+		public float Cost { get; set; }
+
+		/// <summary>
+		/// Gets or sets the cost in words.
+		/// </summary>
+		/// <value>
+		/// The cost in words format.
+		/// </value>
+		[Display(Name ="Cost in Words")]
+		public string CostInWords { get; set; }
+	}
+}
