73fcb3f55563c94b758ab90a6be18f44c9fbf2d2
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2017-11-08 10:16:40 +0800

    Add xml comments for Logic classes

--- src/NameCost.Logic/INameCostLogic.cs
+++ src/NameCost.Logic/INameCostLogic.cs
@@ -2,8 +2,19 @@
 
 namespace NameCost.Logic
 {
+	/// <summary>
+	/// This interface represents any implementation of the logic for the convert number to words in the application
+	/// Via this we adopt strategy pattern so that the logic implementation could be replaced with a new one
+	/// without affecting the rest of the application that use logic
+	/// </summary>
 	public interface INameCostLogic
 	{
+		/// <summary>
+		/// Generates the words for the <see cref="NameCostModel.Cost"/> property of the parameter 
+		/// and stores it in the <see cref="NameCostModel.CostInWords"/> property.
+		/// </summary>
+		/// <param name="nameCost">The model from which <see cref="NameCostModel.Cost"/> will be used 
+		/// to generate and set the <see cref="NameCostModel.CostInWords"/></param>
 		void GenerateWords(NameCostModel nameCost);
 	}
 }
--- src/NameCost.Logic/NameCostLogic.cs
+++ src/NameCost.Logic/NameCostLogic.cs
@@ -2,8 +2,19 @@
 
 namespace NameCost.Logic
 {
+	/// <summary>
+	/// An implementation of the <see cref="NameCost.Logic.INameCostLogic" /> 
+	/// Which generates the words format
+	/// </summary>
+	/// <seealso cref="NameCost.Logic.INameCostLogic" />
 	public class NameCostLogic : INameCostLogic
 	{
+		/// <summary>
+		/// Generates the words for the <see cref="NameCostModel.Cost"/> property of the parameter 
+		/// and stores it in the <see cref="NameCostModel.CostInWords"/> property.
+		/// </summary>
+		/// <param name="nameCost">The model from which <see cref="NameCostModel.Cost"/> will be used 
+		/// to generate and set the <see cref="NameCostModel.CostInWords"/></param>
 		public void GenerateWords(NameCostModel model)
 		{
 			model.CostInWords = new Currency(model.Cost).ToWords();
